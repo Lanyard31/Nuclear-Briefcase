6758ae6ec1cee91ebe5d0b262083632f7b69d3f6
Lanyard <40360728+Lanyard31@users.noreply.github.com>
2018-10-06 02:54:40 +0200

    bullets fire, but no aim

---
+++ Nuke.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+export (int) var speed
+export (int) var damage
+export (float) var lifetime
+
+var velocity = Vector2()
+
+
+func start(_position, _direction):
+	position = _position
+	rotation = _direction.angle() #probably a change here
+	velocity = _direction * speed
+	
+func _process(delta):
+	position += velocity * delta
+
+func explode():
+	queue_free()
+
+func _on_Nuke_body_entered(body):
+	explode()
+	if body.has_method('take_damage'):
+		body.take_damage(damage)
--- main.gd
+++ main.gd
@@ -1,13 +1,43 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+signal shoot
+
+export (PackedScene) var Nuke
+
+var population = 100
+var missilecount = 100
+var can_shoot = true
+var position
+var _position
+var _direction
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
-func _process():
-	pass
+func _process(delta):
+	if Input.is_action_just_pressed("f"):
+		if Input.is_action_pressed("shift"):
+			print("FINLAND")
+			fire_missile()
+			return
+		fire_missile()
+		print("france")
+		
+func fire_missile():
+	var dir = Vector2(1, 0).rotated($PlayerCity/Muzzle.global_rotation) #this is broken because you don't have a turret
+	emit_signal('shoot', Nuke, $PlayerCity/Muzzle.global_position, dir)
+	return
+#		shoot signal leads to on_missile_fire
+	
+func _on_missile_fire(Nuke, _position, _direction):
+	var b = Nuke.instance()
+	add_child(b)
+	b.start(_position, _direction)
+	
+	#subtract one from missilecount
+	#create a missile at origin position
+	#draw a line between origin and target
+	#progress between origin and target
+	#intercept other missiles if there are collisions
+	#explosion anim at target
+	#subtract from missile count and population
